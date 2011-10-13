2da99a2cac7451d2f7c11dc704395575b8b1e922
Juergen Bruester <jb@devilscab.de>
2011-10-13 12:47:12 +0200

    Adds encoding property and default fallback because less engine uses FileWriter in formerly used method which uses default encoding. When using before- or after-elements in css this can result in encoding problems on a website.

--- src/main/java/eu/numberfour/maven/plugins/lesscss/CompileMojo.java
+++ src/main/java/eu/numberfour/maven/plugins/lesscss/CompileMojo.java
@@ -2,15 +2,16 @@ package eu.numberfour.maven.plugins.lesscss;
 
 import com.asual.lesscss.LessEngine;
 import com.asual.lesscss.LessException;
-import java.io.File;
-import java.io.IOException;
-import java.util.Arrays;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.project.MavenProject;
 import org.codehaus.plexus.util.DirectoryScanner;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.util.Arrays;
 
 /**
  * <p>
@@ -69,59 +70,83 @@ public class CompileMojo extends AbstractMojo {
      */
     public boolean caseSensitive;
 
+    /**
+     * Encoding to use for output file.
+     * Defaults to "project.build.sourceEncoding" or platform encoding if not specified.
+     *
+     * @parameter default-value="${project.build.sourceEncoding}"
+     */
+    private String encoding;
+
     public void execute() throws MojoExecutionException, MojoFailureException {
-        try {
-            
-            if(includes == null){
-                includes = new String[1];
-                includes[0]="**/*.less";
-            }
-            
-            Log log = getLog();
-            log.info("");
-            log.info("Creating file list");
-            log.info("Source dir:   " + sourceDir);
-            log.info("Output dir:   " + outputDir);
-            log.info("Includes:     " + Arrays.toString(includes));
-            log.info("Excludes:     " + Arrays.toString(excludes));
+        if(includes == null){
+            includes = new String[1];
+            includes[0]="**/*.less";
+        }
 
-            DirectoryScanner scanner = new DirectoryScanner();
-            scanner.setBasedir(sourceDir);
-            scanner.setIncludes(includes);
-            scanner.setExcludes(excludes);
-            scanner.setCaseSensitive(caseSensitive);
-            scanner.scan();
+        Charset charset = Charset.defaultCharset();
+        if(encoding != null) {
+            charset = Charset.forName(encoding);
+        }
 
-            String[] includedFiles = scanner.getIncludedFiles();
-            
-            log.info("Found " + includedFiles.length + " less files.");
-            log.info("");
-            
-            LessEngine engine = new LessEngine();
-            
-            for (String i:includedFiles){
-                File inputFile = new File(sourceDir+i);
-                File outputFile = new File(outputDir+'/'+i.replaceAll(".less$", ".css"));
-                
-                File outputDir_ = new File(outputFile.getParent());
-                if(!outputDir_.exists()){
-                    outputDir_.mkdirs();
-                }
-                
-                if(log.isDebugEnabled()){
-                    log.debug("Compiling "+inputFile.toString() +" to "+outputFile.toString());
-                }
-                else{
-                    log.info("Compiling "+i);
-                }
-                
-                engine.compile(inputFile, outputFile);
+        Log log = getLog();
+        log.info("");
+        log.info("Creating file list");
+        log.info("Source dir:   " + sourceDir);
+        log.info("Output dir:   " + outputDir);
+        log.info("Includes:     " + Arrays.toString(includes));
+        log.info("Excludes:     " + Arrays.toString(excludes));
+        log.info("Using '" + charset.displayName() + "' encoding for output file(s)");
+
+        DirectoryScanner scanner = new DirectoryScanner();
+        scanner.setBasedir(sourceDir);
+        scanner.setIncludes(includes);
+        scanner.setExcludes(excludes);
+        scanner.setCaseSensitive(caseSensitive);
+        scanner.scan();
+
+        String[] includedFiles = scanner.getIncludedFiles();
+
+        log.info("Found " + includedFiles.length + " less files.");
+        log.info("");
+
+        LessEngine engine = new LessEngine();
+
+        for (String i:includedFiles){
+            File inputFile = new File(sourceDir+i);
+            File outputFile = new File(outputDir+'/'+i.replaceAll(".less$", ".css"));
+
+            File outputDir_ = new File(outputFile.getParent());
+            if(!outputDir_.exists()){
+                outputDir_.mkdirs();
             }
 
-        } catch (IOException ex) {
-            throw new MojoFailureException(ex, "IO Exception", "IO Exception while compiling less files.");
-        } catch (LessException ex) {
-            throw new MojoFailureException(ex, ex.getMessage(), ex.getMessage());
+            if(log.isDebugEnabled()){
+                log.debug("Compiling "+inputFile.toString() +" to "+outputFile.toString());
+            }
+            else{
+                log.info("Compiling "+i);
+            }
+
+            BufferedWriter bw = null;
+            try {
+                String output = engine.compile(inputFile);
+                if (!outputFile.exists()) {
+                    outputFile.createNewFile();
+                }
+                bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outputFile), charset));
+                bw.write(output);
+            } catch (IOException ex) {
+                throw new MojoFailureException(ex, "IO Exception", "IO Exception while compiling less files.");
+            } catch (LessException ex) {
+                throw new MojoFailureException(ex, ex.getMessage(), ex.getMessage());
+            } finally {
+                try {
+                  bw.close();
+                } catch (IOException ex2) {
+                    throw new MojoFailureException(ex2, "IO Exception", "IO Exception while closing output stream.");
+                }
+            }
         }
     }
 }
